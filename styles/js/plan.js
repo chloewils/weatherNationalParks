ac5e2653c5fd3bb11d5a49920d5aa96411e13dc7
Adrein Leavitt <68399114+docl18d@users.noreply.github.com>
2020-10-28 21:23:21 -0600

    js code for park search.  Syill not working

--- styles/js/plan.js
+++ styles/js/plan.js
@@ -1,31 +1,60 @@
-var nationalParkUrl = "developer.nps.gov/api/v1/parks?";
+$(document).$(function () {
+    
 // ajax national park callback
-var nationalParkUrl = "developer.nps.gov/api/v1" + searchTerm;
-var searchTerm = $("#search-value").val();
-    $("#search-value").val("");
-    NationalPark(searchTerm);
-    NationalPark(searchTerm);
+var nationalParkUrl = "developer.nps.gov/api/v1/parks?";
+var searchTerm = $("#search-button").val();
+    $("#search-button").val("");
+    NationalParkFunction(searchTerm);
+   
+    / keyboard 'enter' button event
+    $("#search-button").keypress(function (event) {
+        var keycode = (event.keyCode ? event.keyCode : event.which);
+        if (keycode === 13) {
+            NationalParkFunction(searchTerm);
+
+// Search Storage and history row
+    var history = JSON.parse(localStorage.getItem("history")) || [];
+
+    if (history.length ==< 5) {
+        NationalParkFunction(history[history.length - 1]);
+    }
+    for (var i = 0; i < history.length; i++) {
+        createRow(history[i]);
+    }
+        
+    function createRow(text) {
+        var listItem = $("<li>").addClass("list-group-item").text(text);
+        $(".history").append(listItem);
+    }
+    // list item click functionality listener
+    $(".history").on("click", "li", function () {
+        NationalParkFunction($(this).text());
+        
+});
+
+function weatherFunction(searchTerm) {
 
 var nationalParkApi = function() {  
     $.ajax({
-        url: "https://developer.nps.gov/api/v1/parks?" + "california" + "&api_key=Y0GGEH0NGqggHl4Eyf678f5geglMnqBzFvfV31k7",
+        url: "https://developer.nps.gov/api/v1/parks?" + searchTerm + "&api_key=Y0GGEH0NGqggHl4Eyf678f5geglMnqBzFvfV31k7",
         method: "GET",
-        })
+        }).then(function (response) {
+            if (history.indexOf(searchTerm) === -1) {
+                history.push(searchTerm);
+                localStorage.setItem("history", JSON.stringify(history));
+                createRow(searchTerm);
+                console.log (error);
+            }
 
-        .then(function (response) {
-            console.log (error);
-          });
-        // success: function (result) {
-        //     alert (result.data)
-        // },
-        //     error: function (error) {
-           
-        // }
-    }
+        var title = $("<h3>").addClass("card-title").text(data.name + " (" + new Date().toLocaleDateString() + ")");
+        
+        var lon = data.coord.lon;
+        var lat = data.coord.lat;
 
-// 
+          
 
 
+        
 // api:Y0GGEH0NGqggHl4Eyf678f5geglMnqBzFvfV31k7
 
 // web service request: https://developer.nps.gov/api/v1/parks?&api_key=Y0GGEH0NGqggHl4Eyf678f5geglMnqBzFvfV31k7
