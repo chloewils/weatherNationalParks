da20e2f50f27eb7532a7f40c1a1e5cb5ee337561
Chloe Wilson <chloewilson510@gmail.com>
2020-10-29 19:42:26 -0600

    changed API key and URL, formatted it to lign up correctly

--- styles/js/plan.js
+++ styles/js/plan.js
@@ -1,62 +1,63 @@
 $(document).$(function () {
-    
-// ajax national park callback
-var nationalParkUrl = "developer.nps.gov/api/v1/parks?";
-var searchTerm = $("#search-button").val();
+
+    // ajax national park callback
+    var nationalParkUrl = "https://developer.nps.gov/api/v1/parks?q=Seattle&api_key=DSwWcrFQxBpoiqq12vYeLPVcimOUMrdngmDgePeT&stateCode=WA&limit=5";
+    var searchTerm = $("#search-button").val();
     $("#search-button").val("");
     NationalParkFunction(searchTerm);
-   
+
     / keyboard 'enter' button event
     $("#search-button").keypress(function (event) {
         var keycode = (event.keyCode ? event.keyCode : event.which);
         if (keycode === 13) {
             NationalParkFunction(searchTerm);
 
-// Search Storage and history row
-    var history = JSON.parse(localStorage.getItem("history")) || [];
+            // Search Storage and history row
+            var history = JSON.parse(localStorage.getItem("history")) || [];
 
-    if (history.length ==< 5) {
-        NationalParkFunction(history[history.length - 1]);
-    }
-    for (var i = 0; i < history.length; i++) {
-        createRow(history[i]);
-    }
-        
-    function createRow(text) {
-        var listItem = $("<li>").addClass("list-group-item").text(text);
-        $(".history").append(listItem);
-    }
-    // list item click functionality listener
-    $(".history").on("click", "li", function () {
-        NationalParkFunction($(this).text());
-        
-});
-
-function weatherFunction(searchTerm) {
-
-var nationalParkApi = function() {  
-    $.ajax({
-        url: "https://developer.nps.gov/api/v1/parks?" + searchTerm + "&api_key=Y0GGEH0NGqggHl4Eyf678f5geglMnqBzFvfV31k7",
-        method: "GET",
-        }).then(function (response) {
-            if (history.indexOf(searchTerm) === -1) {
-                history.push(searchTerm);
-                localStorage.setItem("history", JSON.stringify(history));
-                createRow(searchTerm);
-                console.log (error);
+            if (history.length == < 5) {
+                NationalParkFunction(history[history.length - 1]);
             }
 
-        var title = $("<h3>").addClass("card-title").text(data.name + " (" + new Date().toLocaleDateString() + ")");
-        
-        var lon = data.coord.lon;
-        var lat = data.coord.lat;
+            for (var i = 0; i < history.length; i++) {
+                createRow(history[i]);
+            }
 
-          
+            function createRow(text) {
+                var listItem = $("<li>").addClass("list-group-item").text(text);
+                $(".history").append(listItem);
+            }
+            // list item click functionality listener
+            $(".history").on("click", "li", function () {
+                NationalParkFunction($(this).text());
+
+            });
+
+            function nationalParkFunction(searchTerm) {
+
+                var nationalParkApi = function () {
+                    $.ajax({
+                        url: "https://developer.nps.gov/api/v1/parks?q=Seattle&api_key=DSwWcrFQxBpoiqq12vYeLPVcimOUMrdngmDgePeT&stateCode=WA&limit=5" + searchTerm + "&api_key=DSwWcrFQxBpoiqq12vYeLPVcimOUMrdngmDgePeT",
+                        method: "GET",
+                    }).then(function (response) {
+                        if (history.indexOf(searchTerm) === -1) {
+                            history.push(searchTerm);
+                            localStorage.setItem("history", JSON.stringify(history));
+                            createRow(searchTerm);
+                            console.log(error);
+                        }
+
+                        var title = $("<h3>").addClass("card-title").text(data.name + " (" + new Date().toLocaleDateString() + ")");
+
+                        var lon = data.coord.lon;
+                        var lat = data.coord.lat;
 
 
-        
-// api:Y0GGEH0NGqggHl4Eyf678f5geglMnqBzFvfV31k7
 
-// web service request: https://developer.nps.gov/api/v1/parks?&api_key=Y0GGEH0NGqggHl4Eyf678f5geglMnqBzFvfV31k7
+
+
+// api:DSwWcrFQxBpoiqq12vYeLPVcimOUMrdngmDgePeT
+
+// web service request: https://developer.nps.gov/api/v1/parks?q=Seattle&api_key=DSwWcrFQxBpoiqq12vYeLPVcimOUMrdngmDgePeT&stateCode=WA&limit=5
 
 // create an environmental variables (Heroku)
